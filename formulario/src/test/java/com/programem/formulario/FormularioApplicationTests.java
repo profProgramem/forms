363dcddce0df22251aa670a8889777a231463df4
Marcos Limeira <marcos.limeira@programem.dev>
2020-07-16 08:54:19 -0300

    Projeto zerado

---
+++ formulario/src/test/java/com/programem/formulario/FormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programem.formulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
